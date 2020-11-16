779a42817105986d9930a7bcc45161af8dc5963e
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-17 00:20:18 +0100

    feat: make example multiwindow

--- examples/event_loop.ts
+++ examples/event_loop.ts
@@ -1,16 +1,24 @@
 import { serialize } from "../helpers.ts";
 import { EventLoop, Window } from "../mod.ts";
 
-const window = new Window();
+const window1 = new Window();
+const window2 = new Window();
 
 const id = setInterval(() => {
   for (const event of EventLoop.Step()) {
     switch (event.type) {
       case "windowEvent":
-        window.setCursorIcon("hand");
         console.log(serialize(event, 2));
         if (event.value.event.type === "closeRequested") {
           clearInterval(id);
+        }
+
+        if (event.value.event.type === "cursorEntered") {
+          if (event.value.windowId === window1.id) {
+            window1.setCursorIcon("hand");
+          } else {
+            window2.setCursorIcon("crosshair");
+          }
         }
         break;
     }
