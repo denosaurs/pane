e75d8cd42615f98243d53ae857f7e56b3af7a431
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-16 02:42:17 +0100

    chore: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,9 @@ use deno_core::serde_json::Value;
 
 use deno_json_op::json_op;
 
-mod surface;
 mod event;
 mod helpers;
+mod surface;
 
 use surface::Surface;
 
@@ -25,33 +25,33 @@ thread_local! {
 
 #[no_mangle]
 pub fn deno_plugin_init(interface: &mut dyn Interface) {
-  interface.register_op("surface_new", surface_new);
-  interface.register_op("surface_step", surface_step);
+    interface.register_op("surface_new", surface_new);
+    interface.register_op("surface_step", surface_step);
 }
 
 #[json_op]
 fn surface_new(_json: Value, _zero_copy: &mut [ZeroCopyBuf]) -> Result<Value, AnyError> {
-  SURFACE_MAP.with(|cell| {
-    let surface = Surface::new()?;
-    let id = surface.id();
-    cell.borrow_mut().insert(id, surface);
-    Ok(json!(id))
-  })
+    SURFACE_MAP.with(|cell| {
+        let surface = Surface::new()?;
+        let id = surface.id();
+        cell.borrow_mut().insert(id, surface);
+        Ok(json!(id))
+    })
 }
 
 #[json_op]
 fn surface_step(json: Value, _zero_copy: &mut [ZeroCopyBuf]) -> Result<Value, AnyError> {
-  if let Some(id) = json.as_u64() {
-    SURFACE_MAP.with(|cell| {
-      let mut surface_map = cell.borrow_mut();
-      
-      if let Some(surface) = surface_map.get_mut(&id) {
-        Ok(json!(surface.run()))
-      } else {
-        Err(anyhow!("could not find surface {}", id))
-      }
-    })
-  } else {
-    Err(anyhow!("id is none"))
-  }
+    if let Some(id) = json.as_u64() {
+        SURFACE_MAP.with(|cell| {
+            let mut surface_map = cell.borrow_mut();
+
+            if let Some(surface) = surface_map.get_mut(&id) {
+                Ok(json!(surface.run()))
+            } else {
+                Err(anyhow!("could not find surface {}", id))
+            }
+        })
+    } else {
+        Err(anyhow!("id is none"))
+    }
 }
--- src/surface.rs
+++ src/surface.rs
@@ -12,47 +12,47 @@ use crate::event::SurfaceEvent;
 use crate::helpers::hash;
 
 pub struct Surface {
-  pub event_loop: EventLoop<()>,
-  pub window: Window,
+    pub event_loop: EventLoop<()>,
+    pub window: Window,
 }
 
 impl Surface {
-  pub fn new() -> Result<Surface, AnyError> {
-    let event_loop = EventLoop::new();
+    pub fn new() -> Result<Surface, AnyError> {
+        let event_loop = EventLoop::new();
 
-    match Window::new(&event_loop) {
-      Ok(window) => Ok(Surface { event_loop, window }),
-      Err(err) => Err(anyhow!(err)),
+        match Window::new(&event_loop) {
+            Ok(window) => Ok(Surface { event_loop, window }),
+            Err(err) => Err(anyhow!(err)),
+        }
     }
-  }
 
-  pub fn id(&self) -> u64 {
-    hash(self.window.id())
-  }
-
-  pub fn run(&mut self) -> Vec<SurfaceEvent> {
-    let mut events = Vec::new();
-
-    self.event_loop.run_return(|event, _, control_flow| {
-      *control_flow = ControlFlow::Exit;
-
-      events.push(SurfaceEvent::from(event));
-    });
-
-    events
-  }
-
-  pub fn set_title(&self, title: &str) {
-    self.window.set_title(title);
-  }
-
-  pub fn set_icon(&self, rgba: Vec<u8>, width: u64, height: u64) -> Result<(), AnyError> {
-    match Icon::from_rgba(rgba, width as u32, height as u32) {
-      Ok(icon) => {
-        self.window.set_window_icon(Some(icon));
-        Ok(())
-      }
-      Err(err) => Err(anyhow!(err)),
+    pub fn id(&self) -> u64 {
+        hash(self.window.id())
     }
-  }
+
+    pub fn run(&mut self) -> Vec<SurfaceEvent> {
+        let mut events = Vec::new();
+
+        self.event_loop.run_return(|event, _, control_flow| {
+            *control_flow = ControlFlow::Exit;
+
+            events.push(SurfaceEvent::from(event));
+        });
+
+        events
+    }
+
+    pub fn set_title(&self, title: &str) {
+        self.window.set_title(title);
+    }
+
+    pub fn set_icon(&self, rgba: Vec<u8>, width: u64, height: u64) -> Result<(), AnyError> {
+        match Icon::from_rgba(rgba, width as u32, height as u32) {
+            Ok(icon) => {
+                self.window.set_window_icon(Some(icon));
+                Ok(())
+            }
+            Err(err) => Err(anyhow!(err)),
+        }
+    }
 }
